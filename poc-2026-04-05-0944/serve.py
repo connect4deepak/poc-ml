23590371e1f299efc6c77edb1e292231cef5732c
Deepak Kumar <80671306+connect4deepak@users.noreply.github.com>
2026-04-05 10:44:51 +0100

    feat: initial machine learning proof of concept

---
+++ poc-2026-04-05-0944/serve.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+class IrisData(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+app = FastAPI()
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.post('/predict')
+def predict(data: IrisData):
+    prediction = model.predict([[data.sepal_length, data.sepal_width, data.petal_length, data.petal_width]])
+    return {'prediction': prediction[0]}
