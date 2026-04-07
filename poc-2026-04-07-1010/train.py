c880608365986408ea51b34069a7312c361952ee
Deepak Kumar <80671306+connect4deepak@users.noreply.github.com>
2026-04-07 11:11:00 +0100

    feat: add machine learning POC with sklearn classifier and FastAPI inference

---
+++ poc-2026-04-07-1010/train.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pickle
+
+dataset = load_iris()
+X = dataset.data
+y = dataset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print('Model Accuracy:', accuracy_score(y_test, y_pred))
+pickle.dump(model, open('model.pkl', 'wb'))
